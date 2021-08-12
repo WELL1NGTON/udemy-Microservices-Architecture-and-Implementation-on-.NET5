be45de94ba834ff85b4066f9e304c513531bcbdf
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2021-08-12 07:20:13 -0300

    Terminada aula 30

--- 03-Developing-Basket.API-Microservices-with-Redis/AspnetMicroservices/src/Services/Basket/Basket.API/Program.cs
+++ 03-Developing-Basket.API-Microservices-with-Redis/AspnetMicroservices/src/Services/Basket/Basket.API/Program.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
 namespace Basket.API
 {
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
     public class Program
     {
         public static void Main(string[] args)
@@ -16,11 +10,13 @@ namespace Basket.API
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        }
     }
 }
