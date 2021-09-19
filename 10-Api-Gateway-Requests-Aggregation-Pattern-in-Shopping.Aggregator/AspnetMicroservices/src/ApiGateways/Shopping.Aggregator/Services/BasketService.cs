2fef2deef936b76d867dfd3adc590b3f4ab2d72d
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2021-09-19 18:14:04 -0300

    Finalizando a implementação do Aggregator

--- 10-Api-Gateway-Requests-Aggregation-Pattern-in-Shopping.Aggregator/AspnetMicroservices/src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
+++ 10-Api-Gateway-Requests-Aggregation-Pattern-in-Shopping.Aggregator/AspnetMicroservices/src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -1,13 +1,23 @@
 namespace Shopping.Aggregator.Services
 {
+    using System.Net.Http;
     using System.Threading.Tasks;
+    using Shopping.Aggregator.Extensions;
     using Shopping.Aggregator.Models;
 
     public class BasketService : IBasketService
     {
-        public Task<BasketModel> GetBasket(string userName)
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
         {
-            throw new System.NotImplementedException();
+            _client = client ?? throw new System.ArgumentNullException(nameof(client));
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var response = await _client.GetAsync($"/api/v1/Basket/{userName}");
+            return await response.ReadContentAs<BasketModel>();
         }
     }
 }
--- 10-Api-Gateway-Requests-Aggregation-Pattern-in-Shopping.Aggregator/AspnetMicroservices/src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
+++ 10-Api-Gateway-Requests-Aggregation-Pattern-in-Shopping.Aggregator/AspnetMicroservices/src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -1,14 +1,24 @@
 namespace Shopping.Aggregator.Services
 {
     using System.Collections.Generic;
+    using System.Net.Http;
     using System.Threading.Tasks;
+    using Shopping.Aggregator.Extensions;
     using Shopping.Aggregator.Models;
 
     public class OrderService : IOrderService
     {
-        public Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        private readonly HttpClient _client;
+
+        public OrderService(HttpClient client)
         {
-            throw new System.NotImplementedException();
+            _client = client;
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            var response = await _client.GetAsync($"/api/v1/Order/{userName}");
+            return await response.ReadContentAs<List<OrderResponseModel>>();
         }
     }
 }
